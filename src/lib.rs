94db466abde77a8350065db357cdc91a7a7fcc1b
azavgo <azavgo@yahoo.com>
2026-03-19 11:49:52 +1100

    Added checking installed OS functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,30 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::env;
+
+//Types of the currently supported operating systems. Only linux and macos are supported
+//const OS_TYPES: [&str; 3] = ["linux", "macos", "other"];
+pub enum OsTypes {
+    Linux, 
+    Macos, 
+    Other,
+}
+
+//Checks the installed OS
+pub fn which_os() -> OsTypes {
+    let os = env::consts::OS; 
+    match os {
+        "linux" => OsTypes::Linux, 
+        "macos" => OsTypes::Macos, 
+        _       => OsTypes::Other, 
+    }
+}
+
+//Copy utf8 string to linux clipboard
+pub fn clipboard_copy_linux() -> String {
+    match which_os() {
+        OsTypes::Linux => {"linux".to_string()}, 
+        OsTypes::Macos => {"macos".to_string()},
+        OsTypes::Other => {"other".to_string()},
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +32,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn test_clipboard_copy_linux() {
+        assert_eq!(clipboard_copy_linux(), "linux".to_string());
     }
 }
