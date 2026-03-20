f6872459555340174532b6247b57ad7ce8ff9622
azavgo <azavgo@yahoo.com>
2026-03-20 23:18:01 +1100

    Added copy to system clipboard functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,27 @@
 use std::env;
+use crossclip::{Clipboard, SystemClipboard};
+use std::{thread, time::Duration};
+
+//Custom error
+#[derive(Debug)]
+pub enum SystemHelpersError { 
+    ClipboardError(crossclip::ClipboardError),   
+}
+
+impl From<crossclip::ClipboardError> for SystemHelpersError {
+    fn from(error: crossclip::ClipboardError) -> Self {
+        SystemHelpersError::ClipboardError(error)
+    }
+}
+
 
 //Types of the currently supported operating systems. Only linux and macos are supported
 //const OS_TYPES: [&str; 3] = ["linux", "macos", "other"];
 pub enum OsTypes {
     Linux, 
     Macos, 
+    Windows, 
+    FreeBSD,
     Other,
 }
 
@@ -12,19 +29,21 @@ pub enum OsTypes {
 pub fn which_os() -> OsTypes {
     let os = env::consts::OS; 
     match os {
-        "linux" => OsTypes::Linux, 
-        "macos" => OsTypes::Macos, 
-        _       => OsTypes::Other, 
+        "linux"   => OsTypes::Linux, 
+        "macos"   => OsTypes::Macos,
+        "windows" => OsTypes::Windows, 
+        "freebsd" => OsTypes::FreeBSD,
+        _         => OsTypes::Other, 
     }
 }
 
-//Copy utf8 string to linux clipboard
-pub fn clipboard_copy_linux() -> String {
-    match which_os() {
-        OsTypes::Linux => {"linux".to_string()}, 
-        OsTypes::Macos => {"macos".to_string()},
-        OsTypes::Other => {"other".to_string()},
-    }
+//Copy utf8 string to system clipboard using https://crates.io/crates/crossclip 
+//crossclip has been tested for Linux, MacOS, Windows, and FreeBSD
+pub fn copy_to_clipboard(s: String) -> Result<(), SystemHelpersError> {
+    let clipboard = SystemClipboard::new()?;
+    clipboard.set_string_contents(s)?;
+    thread::sleep(Duration::from_millis(10000));
+    Ok(())
 }
 
 #[cfg(test)]
@@ -32,7 +51,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_clipboard_copy_linux() {
-        assert_eq!(clipboard_copy_linux(), "linux".to_string());
+    fn test() {
+        assert_eq!(2, 2);
     }
 }
